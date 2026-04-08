643db1cefcaf7efed8da08567aa57541abfbdd9d
m1racle808 <liakun.jr@gmail.com>
2026-04-08 16:40:08 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,21 @@
+from telegram.ext import ConversationHandler
 
+# Состояния для OSINT-отчёта
+(
+    REPORT_TYPE,
+    REPORT_PERSON_NAME,
+    REPORT_PERSON_NICK,
+    REPORT_PERSON_CITY,
+    REPORT_COMPANY_NAME,
+    REPORT_COMPANY_INN,
+    REPORT_COMPANY_SITE,
+    REPORT_PROJECT_NAME,
+    REPORT_PROJECT_REPO,
+    REPORT_PROJECT_DOMAIN
+) = range(10)
+
+# Состояния для логической цепочки
+(
+    CHAIN_START_INPUT,
+    CHAIN_ACTIONS
+) = range(2)
